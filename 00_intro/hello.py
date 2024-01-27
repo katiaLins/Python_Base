f4449567b1576ac5fc40598f5ffb650a2244b3cb
katiaLins <kmeloal@gmail.com>
2024-01-27 15:38:06 -0300

    Atualizando pastas

---
+++ 00_intro/hello.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python3
+"""Hello word mult Linguas.
+
+Dependendo da ligua configurada no ambiente o programa exibe mesagem
+correspondente.
+Como Usar: 
+
+Tenha a várial LANG devidademente configurada.
+    export LANG=pt_BR
+Ou informar atraves do CLI argument `--lang`
+Ou o usuário terá que digitar.
+
+Execução: 
+    python hello.py
+    ou
+    ./hello.py
+"""
+__version__ = "0.1.3"
+_autor__ = "katia Lins"
+__license__ = "Unlicence"
+
+import os
+import sys
+
+arguments = {"lang" : None,"count" : 1}
+
+for arg in sys.argv[1:]:
+    # TODO: Tratar ValueError
+    key, value = arg.split("=")
+    key = key.lstrip("-").strip()
+    value = value.strip()
+    if key not in arguments:
+        print(f"Ivalid Options `{key}`")
+        sys.exit()
+    arguments[key] = value
+
+current_language = arguments["lang"]
+if current_language is None:
+    # TODO: Usar repetição
+    if "LANG" in os.environ:
+        current_language = os.getenv("LANG")
+    else:
+        current_language = input("Choose a language?")
+
+    
+current_language = current_language[:5]
+
+msg = {
+    "en_US": "Hello, World!",
+    "pt_BR": "Olá Mundão!",
+    "it_IT": "Ciao, Mondo!",
+    "es_SP": "Hola, Mundo!",
+    "fr_FR": "Bonjour, Mode!",
+}
+print(msg[current_language] * int(arguments["count"]))
