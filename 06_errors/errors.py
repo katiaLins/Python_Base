d705151d5b363a38be1b4d56fa29c61ce7314e3c
katiaLins <kmeloal@gmail.com>
2024-01-27 21:40:18 -0300

    Atualizando

--- 06_errors/errors.py
+++ 06_errors/errors.py
@@ -5,17 +5,29 @@ Tratamento de erros em Python.
 import os
 import sys 
 
-if os.path.exists("names.txt"):
-    input("...") # Race Condition
+# Abordagem de tratamento de erros recomendada, pythônica!
+# EAFP - Easy to ASK Forgiveness tha permission
+#       (É mais facil pedir perdão do que permissão)
+
+"""
+try:
+    raise RuntimeErro("Ocoorreu um erro") # Apresenta um erro por conta própia.
+except Exception as e:
+    print(str(e))
+    
+"""
+
+    
+try:# tradução de try: " tente"
     names = open("names.txt").readlines()
-else: 
-     print("[Error]: File names.txt not found.")
+except FileNotFoundError as e: # as e = captura de erro 
+     print(f"{str(e)}.")
      sys.exit(1)
-
-# LBYL - Look Before You Leap
-
-if len(names) >= 3:
-    print(names[1])
-else: 
+     # TODO: Usar retry
+finally: # Vai ser executado independente do erros 
+    print("Execute isso sempre")
+try:
+    print(names[8])
+except: # Bare excep, ignora qalquer tipo de erro.
     print("[Error]: Mising name in the list")
     sys.exit(1)
